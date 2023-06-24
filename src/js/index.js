6aed34941fc780ead4c97dba43a31285e5a96179
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2023-06-24 01:13:52 +0000

    first attempt

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,24 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return (<div className="BigCounterContainer">
+        <div></div>
+        <div className="Clock">
+        <FontAwesomeIcon icon={faClock} />
+            </div>
+        <div className="HundredThousands">6</div>
+        <div className="TenThousands">5</div>
+        <div className="Thousands">4</div>
+        <div className="Hundreds">3</div>
+        <div className="Tenths">2</div>
+        <div className="Units">1</div>
+        </div>)
+}
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
